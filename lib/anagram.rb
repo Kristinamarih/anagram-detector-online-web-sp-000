9479ef42659851a7cd33df88c233b2f3eb989aaf
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-04-28 21:26:34 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   
   def match(array) 
     array.select do |some_word|
-      if some_word.split("").sort == @word.split("").sort
+      some_word.split("").sort == @word.split("").sort
         some_word
       else
         []
